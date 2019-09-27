763d44381ecee3d98ef0ab27583172251da11c91
AustinMcCowan <amccowan@stcharlessd.org>
2019-09-27 12:20:55 -0500

    Adds efficiency to unit attacks inside the attack function Adds unit efficiency values inside the unitObject class Rounds all values that are printed/important to two digit places from .

--- formula.py
+++ formula.py
@@ -5,51 +5,115 @@
 attacks are a scenario between two units, and will consist of multiple conditions. Both objects will have stats affected
 based on the stats of the other. '''
 
+'''>>>ATTEMPT TO SEGMENT UNITS TO HAVE EFFICIENCY TO CERTAIN TYPES'''
 # object to represent unit
 class unitObject:
     # basic stats
-    def __init__(unit, damage, health, atk, title):
-        unit.damage = damage #Base damage. Will later change to be adjusted when implementing unit types.
+    def __init__(unit, damage, health, atk, title, airEF, heliEF, armorEF, infEF):
+        unit.damage = 1
         unit.health = 10
         unit.atk = (unit.health/10)*(unit.damage)
-        unit.title = title #string, unit name.
-        
+        unit.title = title
+        # Efficiency will scale from 0 (can't damage) to 5 ( Max damage )
+        unit.airEF = 0
+        unit.heliEF = 0 
+        unit.infEF = 0 
+        unit.armorEF = 0
+        if unit.title == "tank":
+            unit.airEF = 0
+            unit.heliEF = 2
+            unit.armorEF = 3
+            unit.infEF = 3 
+        if unit.title == "infantry":
+            unit.airEF = 0
+            unit.heliEF = 1
+            unit.armorEF = 1
+            unit.infEF = 2
+    # Will create a unit based on type, then will set a title to the unit based on the type and current iteration of that unit type.
+    def unitCR8(self, title):
+         pass
+     
+     
 # build attack initiate
 def attack(self, target):
-    # print/return will change later on
-    print(target.title, "Health:", target.health,"|", target.title, "damage:", target.atk)
-    print(self.title, "Health:", self.health, "|", self.title, "damage:", self.atk)
-    print(self.title, "attack", target.title)        
-    target.health = (target.health - self.atk)
-    target.atk = (target.health/10)*(target.damage)
-    # If the target unit survives, it counterattacks
-    if target.health > 0:
-        target.atk = (target.health/10)*(target.damage)
-        print(target.title, "Health:", target.health,"|", target.title, "damage:", target.atk)
-        print(target.title, "counterattacks", self.title)
-        self.health = (self.health - target.atk)
-        self.atk = (self.health/10)*(self.damage)
-        # if the attacking unit doesn't survive the counterattack, it is instead destroyed
-        if self.health > 0:
-            print(self.title, "Health:", self.health, "|", self.title, "damage:", self.atk)
-            print("ENDSCENARIO=========")
-        else:
-            print("Counterattack was lethal,", self.title, "destroyed")
-            print("ENDSCENARIO=========")
-            del self        
+    # CATEGORIZING DAMAGE EFFICIENCY BETWEEN UNITS      
+    if target.health <= 0:
+        pass
+    elif self.health <= 0:
+        pass
     else:
-        print(target.title, "was destroyed")
-        print("ENDSCENARIO=========")
-        del target
+        if self.title == "tank":
+            if target.title == "infantry":
+                self.damage = self.infEF
+                target.damage = target.armorEF
+                self.atk = (self.health/10)*(self.damage)
+                self.atk = round(self.atk, 2)
+                target.atk = (target.health/10)*(target.damage)
+                target.atk = round(target.atk, 2)
+            elif target.title == "tank":
+                self.damage = self.armorEF
+                target.damage = target.armorEF
+                self.atk = (self.health/10)*(self.damage)
+                self.atk = round(self.atk, 2)
+                target.atk = (target.health/10)*(target.damage)
+                target.atk = round(target.atk, 2)
+            else:
+                pass
+        if self.title == "infantry":
+            if target.title == "infantry":
+                self.damage = self.infEF
+                target.damage = target.infEF               
+                self.atk = (self.health/10)*(self.damage)
+                self.atk = round(self.atk, 2)                
+                target.atk = (target.health/10)*(target.damage)
+                target.atk = round(target.atk, 2)
+            elif target.title == "tank":
+                self.damage = self.armorEF
+                target.damage = target.infEF              
+                self.atk = (self.health/10)*(self.damage)
+                self.atk = round(self.atk, 2)              
+                target.atk = (target.health/10)*(target.damage)
+                target.atk = round(target.atk, 2)
+            else:
+                pass        
+        # print/return will change later on
+        # stats starting:
+        print(self.title, "Health:", self.health, "|", self.title, "damage:", self.atk)
+        print(target.title, "Health:", target.health,"|", target.title, "damage:", target.atk)
+        print(self.title, "attacks", target.title)
+        # Actual attack and target unit getting stats adjusted
+        target.health = (target.health - self.atk)
+        target.health = round(target.health, 2)
+        target.atk = (target.health/10)*(target.damage)
+        target.atk = round(target.atk, 2)
+        # If the target unit survives, it counterattacks
+        if target.health > 0:
+            target.atk = (target.health/10)*(target.damage)
+            target.atk = round(target.atk, 2)
+            print(target.title, "Health:", target.health,"|", target.title, "damage:", target.atk)
+            print(target.title, "counterattacks", self.title)
+            self.health = (self.health - target.atk)
+            self.health = round(self.health, 2)
+            self.atk = (self.health/10)*(self.damage)
+            self.atk = round(self.atk, 2)
+            # if the attacking unit doesn't survive the counterattack, it is instead destroyed
+            if self.health > 0:
+                print(self.title, "Health:", self.health, "|", self.title, "damage:", self.atk)
+                print("ENDSCENARIO=========")
+            else:
+                print("Counterattack was lethal,", self.title, "destroyed")
+                print("ENDSCENARIO=========")
+                del self        
+        else:
+            print(target.title, "was destroyed")
+            print("ENDSCENARIO=========")
+            del target
             
-# tester units, first value = attack value
-unit1 = unitObject(5.0, 0, 0, "blue")
-unit2 = unitObject(5.0, 0, 0, "red")
-
-
 # attack function, can be used universally
-attack(unit1, unit2)
-attack(unit2, unit1)
-attack(unit1, unit2)
-attack(unit2, unit1)
-
+if __name__ == "__main__":
+    unit1 = unitObject(0, 0, 0, "tank", 0, 0, 0, 0)
+    unit2 = unitObject(0, 0, 0, "infantry", 0, 0, 0, 0)
+    for i in range(20):
+        attack(unit1, unit2)
+        attack(unit2, unit1)
+    
